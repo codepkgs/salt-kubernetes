81c96b2fe0cdec2a812d0dc1c70c1bdd17fa0217
zhanghe <zhanghe@maka.im>
2020-10-20 16:46:49 +0800

    增加ingress-nginx

--- scripts/addons.sh
+++ scripts/addons.sh
@@ -39,6 +39,17 @@ deploy_metrics_server() {
     $KUBECTL apply -f ${addon_dir}/
 }
 
+deploy_ingress_nginx() {
+    local addon_dir='addons/ingress-nginx'
+    echo
+    echo "deploy ingress-nginx ......"
+
+    $KUBECTL apply -f ${addon_dir}/ingress-nginx-deploy.yaml
+
+    echo "then invoke command to set label <ingress: true> for node"
+    echo "set label command: $0 ingress-label <node_name>"
+}
+
 taint_master() {
     for i in `seq 1 ${MASTER_COUNTS}`
     do
@@ -55,13 +66,34 @@ master_label() {
     done
 }
 
+ingress_label() {
+    local ops=$1
+    local node=$2
+    if [ -n "$node" ]; then
+        if [ "$ops" == 'do' ]; then
+            $KUBECTL label node $node ingress=true
+        elif [ "$ops" == 'undo' ]; then
+            $KUBECTL label node $node ingress-
+        fi
+    else
+        echo "please specify the node name!"
+        exit 0
+    fi
+}
+
 help() {
-    echo "usage: $0 {flannel|coredns|metrics-server|taint-master|master-label}"
-    echo -e "\t$0 flannel\t\t:部署 flannel"
-    echo -e "\t$0 coredns\t\t:部署 coredns"
-    echo -e "\t$0 metrics-server\t:部署 metrics-server"
-    echo -e "\t$0 taint-master\t:给 master 节点设置污点"
-    echo -e "\t$0 master-label\t:给 master 节点设置 label，打上 master 标签"
+    echo "usage:"
+    echo -e "\t$0 {flannel|coredns|metrics-server|ingress-nginx|taint-master|master-label|ingress-label|undo-ingress-label}"
+    echo
+    echo "summary:"
+    echo -e "\t$0 flannel\t\t\t\t:部署 flannel"
+    echo -e "\t$0 coredns\t\t\t\t:部署 coredns"
+    echo -e "\t$0 metrics-server\t\t\t:部署 metrics-server"
+    echo -e "\t$0 ingress-nginx\t\t\t:部署 ingress-nginx"
+    echo -e "\t$0 taint-master\t\t\t:给 master 节点设置污点"
+    echo -e "\t$0 master-label\t\t\t:给 master 节点设置 label，打上 master 标签"
+    echo -e "\t$0 ingress-label <node_name>\t\t:给节点设置 ingress:true 的标签，否则 ingress-nginx-controller 无法创建"
+    echo -e "\t$0 undo-ingress-label <node_name>\t:删除节点的 ingress:true 的标签，会删除 ingress-nginx-controller 容器"
     exit 0
 }
 
@@ -74,6 +106,15 @@ case $1 in
         ;;
     metrics-server)
         deploy_metrics_server
+        ;;
+    ingress-nginx)
+        deploy_ingress_nginx
+        ;;
+    ingress-label)
+        ingress_label do $2
+        ;;
+    undo-ingress-label)
+        ingress_label undo $2
         ;;
     taint-master)
         taint_master
